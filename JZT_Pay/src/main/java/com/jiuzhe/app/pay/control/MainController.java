a9bb1327cf0f7fffbac7227f2f57411bcc6f4011
weix <xnn_001@163.com>
2018-05-17 17:41:54 +0800

    weix

--- JZT_Pay/src/main/java/com/jiuzhe/app/pay/control/MainController.java
+++ JZT_Pay/src/main/java/com/jiuzhe/app/pay/control/MainController.java
@@ -53,6 +53,58 @@ public class MainController {
     @Autowired
     MysqlTransactionService mysqlTx;
 
+    private String getIpAddress(HttpServletRequest request) throws IOException {  
+        // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址  
+  
+        String ip = request.getHeader("X-Forwarded-For");  
+        if (logger.isInfoEnabled()) {  
+            logger.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);  
+        }  
+  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+                ip = request.getHeader("Proxy-Client-IP");  
+                if (logger.isInfoEnabled()) {  
+                    logger.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);  
+                }  
+            }  
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+                ip = request.getHeader("WL-Proxy-Client-IP");  
+                if (logger.isInfoEnabled()) {  
+                    logger.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);  
+                }  
+            }  
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+                ip = request.getHeader("HTTP_CLIENT_IP");  
+                if (logger.isInfoEnabled()) {  
+                    logger.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);  
+                }  
+            }  
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+                if (logger.isInfoEnabled()) {  
+                    logger.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);  
+                }  
+            }  
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+                ip = request.getRemoteAddr();  
+                if (logger.isInfoEnabled()) {  
+                    logger.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);  
+                }  
+            }  
+        } else if (ip.length() > 15) {  
+            String[] ips = ip.split(",");  
+            for (int index = 0; index < ips.length; index++) {  
+                String strIp = (String) ips[index];  
+                if (!("unknown".equalsIgnoreCase(strIp))) {  
+                    ip = strIp;  
+                    break;  
+                }  
+            }  
+        }  
+        return ip;  
+    }  
+
     private Map<String,String> getRequestParam(HttpServletRequest request) {
     	Map<String,String> params = new HashMap<String,String>();
 		Map requestParams = request.getParameterMap();
@@ -73,6 +125,7 @@ public class MainController {
 
     private Map<String,String> getWeChatPayReturn(HttpServletRequest request) {
     	try {
+    		logger.info(getIpAddress(request));
             InputStream inStream = request.getInputStream();
             int _buffer_size = 4096;
             if (inStream != null) {
